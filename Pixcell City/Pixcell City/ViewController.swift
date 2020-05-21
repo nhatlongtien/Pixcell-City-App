ac81fc7d56d633bbf633aea5c2b768d4b581639e
Tien Nguyen Nhat <anonymous209184@gmail.com>
2020-05-21 16:10:30 +0700

    end of lesson 2

---
+++ Pixcell City/Pixcell City/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pixcell City
+//
+//  Created by NGUYENLONGTIEN on 5/21/20.
+//  Copyright © 2020 NGUYENLONGTIEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pixcell City/Pods/Target Support Files/Pods-Pixcell City/Pods-Pixcell City-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pixcell_City : NSObject
+@end
+@implementation PodsDummy_Pods_Pixcell_City
+@end
